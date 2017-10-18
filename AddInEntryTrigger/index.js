6bec9ffc9f21c6c770a4ce6d51f5b93ec7e312db
sebug <sebastian.gfeller@gmail.com>
2017-10-18 15:10:28 +0200

    Verifying JWT

--- AddInEntryTrigger/index.js
+++ AddInEntryTrigger/index.js
@@ -1,14 +1,25 @@
 var jwt = require('jsonwebtoken');
+var request = require('request');
+
+var keysUrl = 'https://login.microsoftonline.com/sebutech.onmicrosoft.com/discovery/v2.0/keys?p=b2c_1_siupin';
 
 module.exports = function (context, req) {
     context.log('Requested Add-in entry.');
 
-    var decoded = jwt.decode(req.headers['x-ms-token-aad-id-token']);
+    var token = req.headers['x-ms-token-aad-id-token'];
+    context.log(token);
 
-    context.log(JSON.stringify(decoded));
+    request(keysUrl, function (error, response, body) {
+	context.log('Keys content');
+	context.log(body);
 
-    context.res = {
-	body: "Oh, hey world"
-    };
-    context.done();
+	var decoded = jwt.decode(token);
+
+	context.log(JSON.stringify(decoded));
+
+	context.res = {
+	    body: "Oh, hey world"
+	};
+	context.done();
+    });
 };
