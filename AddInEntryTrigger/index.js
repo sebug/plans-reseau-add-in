991ac3372936adc2f139859fff07423f17516ced
sebug <sebastian.gfeller@gmail.com>
2017-10-18 15:27:33 +0200

    With verify logic

--- AddInEntryTrigger/index.js
+++ AddInEntryTrigger/index.js
@@ -7,22 +7,41 @@ module.exports = function (context, req) {
     context.log('Requested Add-in entry.');
 
     var token = req.headers['x-ms-token-aad-id-token'];
-    context.log(token);
 
     request(keysUrl, function (error, response, body) {
 
 	var decoded = jwt.decode(token, { complete: true });
 
+	var kid = decoded.header.kid;
+
 	var keysObj = JSON.parse(body);
-	var k = keysObj.keys[0].n;
+	var k = keysObj.keys.filter(function (k2) {
+	    k2.kid == kid;
+	});
 
-	
+	if (!k) {
+	    context.log('Was not able to find key to validate JWT signature');
+	    context.res = {
+		status: 500,
+		body: 'Was not able to find key to validate JWT signature'
+	    };
+	    context.done();
+	} else {
+	    try {
+		decoded = jwt.verify(token, k.n);
+	    } catch (e) {
+		context.log(JSON.stringify(e));
+		context.res = {
+		    status: 500,
+		    body: JSON.stringify(e)
+		};
+	    }
+	    context.log(JSON.stringify(decoded));
 
-	context.log(JSON.stringify(decoded));
-
-	context.res = {
-	    body: "Oh, hey world"
-	};
-	context.done();
+	    context.res = {
+		body: "Oh, hey world"
+	    };
+	    context.done();
+	}
     });
 };
